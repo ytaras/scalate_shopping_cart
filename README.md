08fd865adec6fce42098442747c5048517307f7f
Yura Taras <yura.taras@gmail.com>
2013-04-03 18:21:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Scalatra Shopping Cart #
+
+## Build & Run ##
+
+```sh
+$ cd Scalatra_Shopping_Cart
+$ ./sbt
+> container:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/com/softserve/cart/MyScalatraServlet.scala
@@ -0,0 +1,17 @@
+package com.softserve.cart
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class MyScalatraServlet extends ScalatraShoppingCartStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}

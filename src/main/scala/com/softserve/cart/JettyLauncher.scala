f57d56be3af68519781116bc3aa78de8566880f2
Yura Taras <yura.taras@gmail.com>
2013-04-08 13:18:04 +0300

    Update JettyLauncher.scala

--- src/main/scala/com/softserve/cart/JettyLauncher.scala
+++ src/main/scala/com/softserve/cart/JettyLauncher.scala
@@ -4,7 +4,8 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.webapp.WebAppContext
 
-object JettyLauncher { // this is my entry object as specified in sbt project definition
+// Copied from http://www.scalatra.org/2.2/guides/deployment/standalone.html
+object JettyLauncher {
   def main(args: Array[String]) {
     val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
 
